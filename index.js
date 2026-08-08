d3af7bf362f871289eeb234bb2e0f8c951031bc7
sahil <sayyed@Anonymous.local>
2026-08-08 21:25:27 +0530

    Use routes and query parameters for dynamic routing

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
-app.use(express.json());
+app.get('/welcome/:username', (req, res) => {
+    const username = req.params.username;
+    const role = req.query.role;
+    
+    res.send(`Welcome ${username}, your role is ${role}`);
+})
 
-// GET /products
-app.get("/products", (req, res) => {
-  res.send("Here is the list of all products.");
-});
-
-
-// POST /products
-app.post("/products", (req, res) => {
-  res.send("A new product has been added.");
-});
-
-// GET /categories
-app.get("/categories", (req, res) => {
-  res.send("Here is the list of all categories.");
-});
-
-
-// POST /categories
-app.post("/categories", (req, res) => {
-  res.send("A new category has been created.");
-});
-
-// Wildcard route for undefined routes
-app.use("*", (req, res) => {
-  res.status(404).send("<h1>404 - Page Not Found</h1>");
-});
-
-
-// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
