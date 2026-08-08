17a5ffdb1c934c6a2b1e9214e06ceef60a787f96
sahil <sayyed@Anonymous.local>
2026-08-08 20:41:19 +0530

    Adding custom middleware and routing

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ import  express from "express";
 const app = express();
 const PORT = 3000;
 
+app.use((req, res, next) => {
+  req.user = "Fuuggi";
+  next();
+});
+
+app.get("/welcome", (req, res) => {
+  res.send(`<h1>Welcome, ${req.user}!</h1>`);
+}); 
 
 app.listen(PORT, () => {    
   console.log(`Server is up and running on port ${PORT}! Ready to handle requests.`);
