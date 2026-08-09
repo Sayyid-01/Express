2e0388927a58189022484e03fc3bb15365910f2b
sahil <sayyed@Anonymous.local>
2026-08-09 15:25:09 +0530

    An Ecommerce Startup

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
 import express from "express";
-import courseRoutes from "./routes/course.js";
-import studentRoutes from "./routes/students.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Student and Course API!');
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the E-Commerce API");
 });
 
-app.use('/courses', courseRoutes);
-app.use('/students', studentRoutes);
-
-app.use((req, res) => {
-    res.status(404).send('Page not found.');
-});
+app.use("/carts", cartRoutes);
+app.use("/products", productRoutes);
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
---
+++ routes/cartRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/:userId", (req, res) => {
+    const userId = req.params.userId;
+    res.send(`Fetching carts for user with ID: ${userId}`);
+});
+
+router.post("/:userId", (req, res) => {
+    const userId = req.params.userId;
+    res.send(`Adding product to cart for user with ID:${userId}`);
+});
+
+
+export default router;
