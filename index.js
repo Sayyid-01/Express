dca62b4cfdd2c80625761f3f2df505a235e891f4
sahil <sayyed@Anonymous.local>
2026-08-09 12:58:28 +0530

    Create a Server with Middleware for Logging

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
-app.get('/welcome/:username', (req, res) => {
-    const username = req.params.username;
-    const role = req.query.role;
-    
-    res.send(`Welcome ${username}, your role is ${role}`);
-})
+// Logging Middleware
+app.use((req,res,next)=>{
+    console.log(`${req.method} request made to ${req.url}`);
+    next();
+});
+
+app.get('/products', (req, res) => {
+    res.send("Here is the list of all products.");
+});
+app.post('/products', (req, res) => {
+    res.send("A new product has been added.");
+});
+
+app.get('/categories', (req, res) => {
+    res.send("Here is the list of all categories.");
+});
+
+app.post('/categories', (req, res) => {
+    res.send("A new category has been created.");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
